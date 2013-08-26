ca5a7423e27a497669932611f07561dfa6d0d462
Weihong Guan <weihong.guan@gmail.com>
2013-08-27 01:05:46 +0800

    rename test script and fix spell error of accelerometer

--- src/node.cpp
+++ src/node.cpp
@@ -14,7 +14,7 @@ Gpio i2c_scl(GPIOB, GPIO_Pin_6, RCC_APB2Periph_GPIOB);
 Gpio i2c_sda(GPIOB, GPIO_Pin_7, RCC_APB2Periph_GPIOB);
 
 I2c i2c(I2C1, RCC_APB1Periph_I2C1);
-Adxl345 acclerator(i2c);
+Adxl345 accelerometer(i2c);
 
 Node::Node(UsartRs485Modbus & usart, uint8_t address) :
 		SlaveRtu(usart, address) {
@@ -33,15 +33,15 @@ void Node::init() {
 	i2c_sda.init(GPIO_Mode_AF_OD);
 	i2c.init(I2C_Mode_I2C, 400000);
 
-	acclerator.init();
+	accelerometer.init();
 }
 
 uint8_t Node::updateShortInputs(uint16_t index, uint16_t length) {
 
 	for (uint16_t i = 0; i < length; i++) {
-		acclerator.measure();
+		accelerometer.measure();
 		this->setShortInput(index + i,
-				*(acclerator.getAcceleratins() + index + i));
+				*(accelerometer.getAcceleratins() + index + i));
 	}
 
 	return 0;
--- test/md_accelerometer.py
+++ test/md_accelerometer.py
@@ -6,11 +6,14 @@ node.serial.parity = 'E'
 
 #node.debug = True
 
-x = node.read_register(0, 0, 4, True)
-y = node.read_register(1, 0, 4, True)
-z = node.read_register(2, 0, 4, True)
-
-print "x: %d, y: %d, z: %d" % (x, y, z)
+print "Adxl345: "
+i = 10;
+while (i > 0):
+	x = node.read_register(0, 0, 4, True)
+	y = node.read_register(1, 0, 4, True)
+	z = node.read_register(2, 0, 4, True)
+	print "x: %+3d, y: %+3d, z: %+3d" % (x, y, z)
+	i -= 1;
 
 '''
 02050000ff008C09
